bd2eeecdeaec66a0fbe148b5412018e617c014cf
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2021-11-02 12:17:14 -0300

    Move memory footprint measurement to main file

--- main.go
+++ main.go
@@ -5,8 +5,26 @@ import (
 	"fmt"
 	"jeangnc/pattern-matcher/pkg/persistency"
 	"log"
+	"runtime"
 	"time"
 )
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	fmt.Printf("\n")
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
 
 func main() {
 	filename := flag.String("filename", "", "The file containing a list of conditions")
@@ -20,6 +38,8 @@ func main() {
 	fmt.Println("Loading tree")
 	persistency.LoadJsonFile(*filename)
 	fmt.Println("Initialization time:", time.Since(start))
+
+	printMemUsage()
 
 	time.Sleep(60 * time.Second)
 }
--- pkg/persistency/filesystem.go
+++ pkg/persistency/filesystem.go
@@ -2,7 +2,6 @@ package persistency
 
 import (
 	"encoding/json"
-	"fmt"
 	"jeangnc/pattern-matcher/pkg/tree"
 	"jeangnc/pattern-matcher/pkg/types"
 	"log"
@@ -28,24 +27,6 @@ func LoadFile(filename string) *tree.ConditionTree {
 
 	// FIXME: remove after tuning memory footprint
 	runtime.GC()
-	printMemUsage()
 
 	return t
-}
-
-// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
-// of garage collection cycles completed.
-func printMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-	fmt.Printf("\n")
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
