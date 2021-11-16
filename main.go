9158beffe22a8873375391dd1199e1bb4fd10a06
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2021-11-15 22:05:31 -0300

    Move utility funcs to another pkg

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"jeangnc/event-stream-filter/pkg/persistency"
 	"jeangnc/event-stream-filter/pkg/persistency/filesystem"
 	"jeangnc/event-stream-filter/pkg/server"
 	"log"
-	"runtime"
 )
 
 const (
@@ -23,23 +21,6 @@ func buildAdapter(o Options) persistency.Adapter {
 	}
 
 	return filesystem.NewFilesystemAdapter(o.filename)
-}
-
-// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
-// of garage collection cycles completed.
-func printMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-	fmt.Printf("\n")
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
 }
 
 func main() {
