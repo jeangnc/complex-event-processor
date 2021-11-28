6c16bf4ac5705eb181365c6057a831d38f70fb2b
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2021-11-28 12:05:06 -0300

    Add public proto representations

---
+++ dist/ruby/lib/event-stream-filter/event.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module EventStreamFilter
+  class Event
+    attr_reader :id, :tenant_id, :kind, :payload
+
+    def initialize(id:, tenant_id:, kind:, payload:)
+      @id = id
+      @tenant_id = tenant_id
+      @kind = kind
+      @payload = payload
+    end
+  end
+end
