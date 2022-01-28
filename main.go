a80f19129e5fb0d074165b11f8ac1c1b254a26a3
Jean Gonçalves <jean.gnc@gmail.com>
2022-01-28 20:04:46 -0300

    --wip-- [skip ci]

--- main.go
+++ main.go
@@ -20,16 +20,34 @@ type ServerImpl struct {
 func (s *ServerImpl) RegisterCondition(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterResponse, error) {
 	s.tree.Append(in.Condition)
 
+	// TODO: keep and index of expressions
+	// TODO: persist expressions
+
 	return &pb.RegisterResponse{}, nil
 }
 
-func (s *ServerImpl) Filter(ctx context.Context, in *pb.FilterRequest) (*pb.FilterResponse, error) {
+func (s *ServerImpl) Process(ctx context.Context, in *pb.ProcessRequest) (*pb.ProcessResponse, error) {
 	conditions := s.tree.Search(in.Event)
 
-	return &pb.FilterResponse{
+	// TODO: impact processing
+	// TODO: persist entities
+	// TODO: notifications as response?
+
+	return &pb.ProcessResponse{
 		Event:      in.Event,
 		Conditions: conditions,
 	}, nil
+}
+
+// TODO: async API
+// TODO: sql interface
+
+func processEvent() {
+	// load entity
+	// iterates over impact evaluation result
+	// mutates the entity
+	// compares the previous and next result
+	// emit notification
 }
 
 func main() {
