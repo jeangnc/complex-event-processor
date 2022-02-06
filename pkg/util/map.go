dcbb783cda2ab08c1cab4c699517098f5eb7f8eb
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2022-02-06 02:43:04 -0300

    Add utility functions to deal with maps

---
+++ pkg/util/map.go
@@ -0,0 +1,20 @@
+package util
+
+func MergeMap[K comparable, V any](m1 map[K]V, m2 map[K]V) map[K]V {
+	newMap := CopyMap(m1)
+	for k, v := range m2 {
+		newMap[k] = v
+	}
+
+	return newMap
+}
+
+func CopyMap[K comparable, V any](m map[K]V) map[K]V {
+	newMap := map[K]V{}
+
+	for k, v := range m {
+		newMap[k] = v
+	}
+
+	return newMap
+}
---
+++ pkg/util/map_test.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Tests whether a set of changes impacted the expression
+func TestMapMerge(t *testing.T) {
+	mapA := map[interface{}]interface{}{
+		"a": true,
+	}
+	mapB := map[interface{}]interface{}{
+		"b": true,
+	}
+	expectedResult := map[interface{}]interface{}{
+		"a": true,
+		"b": true,
+	}
+
+	result := MergeMap(mapA, mapB)
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Fatalf(`Failed to merge %v %v. Result : %v`, mapA, mapB, result)
+	}
+}
+
+// Tests map copying
+func TestMapCopy(t *testing.T) {
+	myMap := map[string]string{
+		"key": "value",
+	}
+
+	copiedMap := CopyMap(myMap)
+	if !reflect.DeepEqual(myMap, copiedMap) {
+		t.Fatalf(`Failed to copy map %v %v`, myMap, copiedMap)
+	}
+}
+
+// Tests map reference
+func TestCopiedMapRefere(t *testing.T) {
+	myMap := map[string]string{
+		"key": "value",
+	}
+
+	copiedMap := CopyMap(myMap)
+	copiedMap["another-key"] = "another-value"
+
+	if reflect.DeepEqual(myMap, copiedMap) {
+		t.Fatalf(`Original map mutated %v %v`, myMap, copiedMap)
+	}
+}
