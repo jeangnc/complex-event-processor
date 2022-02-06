e8f4fbed7a4d2171d5447501c3f0e1e926352e14
Jean Carlos Gonçalves <jean.gnc@gmail.com>
2022-02-06 02:45:01 -0300

    Replace private implementation for utility function

--- pkg/mutation/processing.go
+++ pkg/mutation/processing.go
@@ -1,5 +1,9 @@
 package mutation
 
+import (
+	util "github.com/jeangnc/complex-event-processor/pkg/util"
+)
+
 type Entity struct {
 	predicates map[string]bool
 	relations  map[string]bool
@@ -12,26 +16,7 @@ type Impact struct {
 
 func Process(e Entity, i Impact) Entity {
 	return Entity{
-		predicates: mergeMap(e.predicates, i.predicates),
-		relations:  mergeMap(e.relations, i.relations),
+		predicates: util.MergeMap(e.predicates, i.predicates),
+		relations:  util.MergeMap(e.relations, i.relations),
 	}
-}
-
-func mergeMap(m1 map[string]bool, m2 map[string]bool) map[string]bool {
-	newMap := copyMap(m1)
-	for k, v := range m2 {
-		newMap[k] = v
-	}
-
-	return newMap
-}
-
-func copyMap(m map[string]bool) map[string]bool {
-	newMap := map[string]bool{}
-
-	for k, v := range m {
-		newMap[k] = v
-	}
-
-	return newMap
 }
