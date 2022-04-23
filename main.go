92eb628d240279c2a4731af2bc1c9e4c6626c55d
Jean Gonçalves <jean.gnc@gmail.com>
2022-04-23 11:11:19 -0300

    Undo main impl

--- main.go
+++ main.go
@@ -1,43 +1,8 @@
 package main
 
-import (
-	"context"
-
-	pb "github.com/jeangnc/complex-event-processor/pkg/proto"
-	"github.com/jeangnc/complex-event-processor/pkg/server"
-	"github.com/jeangnc/complex-event-processor/pkg/tree"
-)
-
 const (
 	port = ":8080"
 )
 
-type ServerImpl struct {
-	pb.UnimplementedEventStreamServer
-	tree *tree.ConditionTree
-}
-
-func (s *ServerImpl) RegisterCondition(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	s.tree.Append(in.Condition)
-
-	return &pb.RegisterResponse{}, nil
-}
-
-func (s *ServerImpl) Filter(ctx context.Context, in *pb.FilterRequest) (*pb.FilterResponse, error) {
-	conditions := s.tree.Search(in.Event)
-
-	return &pb.FilterResponse{
-		Event:      in.Event,
-		Conditions: conditions,
-	}, nil
-}
-
 func main() {
-	t := tree.NewConditionTree()
-	i := &ServerImpl{
-		tree: t,
-	}
-
-	s := server.NewGrpcServer(i)
-	s.Start(port)
 }
