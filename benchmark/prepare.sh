fe6c01d070334daf323ddbdfa75320cf61b45055
Jean Gonçalves <jean.gnc@gmail.com>
2023-06-11 22:02:45 -0300

    Commits benchmark data

--- benchmark/prepare.sh
+++ benchmark/prepare.sh
@@ -1,5 +1,17 @@
-sudo sysctl -w net.inet.ip.portrange.first=32768
+# sysctl -w net.inet.ip.portrange.first=32768
 
 curl -X POST localhost:8080/expression \
     -H 'Content-Type: application/json ' \
-    -d @complex-event-1.json
+    -d @queries/query-1.json
+
+curl -X POST localhost:8080/expression \
+    -H 'Content-Type: application/json ' \
+    -d @queries/query-2.json
+
+curl -X POST localhost:8080/expression \
+    -H 'Content-Type: application/json ' \
+    -d @queries/query-3.json
+
+curl -X POST localhost:8080/expression \
+    -H 'Content-Type: application/json ' \
+    -d @queries/query-4.json
